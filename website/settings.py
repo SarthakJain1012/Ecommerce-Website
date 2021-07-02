82a09b007135166c065d05eeac206085dec8d16e
Sarthak <sarthak.jain058@gmail.com>
2021-07-02 22:12:45 +0530

    Tweaked for Heroku4

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-o3z6it$m=8^aa!k_20%f&a6hhu_3qz3w_z60%x2z$k2p&8y^!2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
